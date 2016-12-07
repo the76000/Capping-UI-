7cac1a989ee8496d1816ffb261c0bb65c708d9b6
Colin <colinferris95@gmail.com>
2016-12-06 21:52:07 -0800

    PHP stuff in this file
    DOES NOT WORK NEEDS MORE WORK

--- Assign-Curriculum.php
+++ Assign-Curriculum.php
@@ -12,6 +12,51 @@
 	<link rel="stylesheet" href="CSS/style.css">
 
 	<title>CPCA Enroll Participant</title>
+	<?php
+		
+		session_start();
+	
+		if (!isset($_SESSION["username"]) ){
+			header('Location: index.php');
+			echo "hello";
+		}
+	  
+	  
+		// Connecting, selecting database
+		$dbconn = pg_connect("host=10.10.7.195 port=5432 dbname=cappingdb user=postgres password=admin")
+			or die('Could not connect: ' . pg_last_error());
+			
+			echo "<h1> THIS DOES NOT WORK </h1>";
+		
+		if( isset($_POST['curriculum_select']) && isset($_POST['f_name']) && isset($_POST['l_name'])){
+			
+			$first_name = $_POST['f_name'];
+			$last_name = $_POST['l_name'];
+			$select_value = $_POST['curriculum_select'];
+			
+			$query1 = 'SELECT * FROM referrals WHERE ref_f_name = "'.$first_name.'" AND ref_l_name = '.$last_name;
+			$query2 = 'SELECT cid FROM curriculum WHERE curriculum_name = '.$select_value;
+			
+			echo $query1;
+			
+			$p_num_result = pg_query($query1) or die('Query failed: ' . pg_last_error());
+			$cid_result = pg_query($query2) or die('Query failed: ' . pg_last_error());
+			
+			echo $p_num_result;
+			echo $cid_result;
+			
+			$p_num = pg_fetch_array($p_num_result);
+			$cid = pg_fetch_array($cid_result);
+			
+			echo $p_num;
+			echo $cid;
+			
+			$query = 'UPDATE participants SET cid = '.$cid.' WHERE p_num = '.$p_num;
+			
+			pg_query($query) or die('Query failed: ' . pg_last_error());
+			
+		}
+	?>
 </head>
 <body>
 <!-- Top left Logo -->
@@ -50,7 +95,7 @@
 <!-- css div's for jumbotron work best --> 
 <!-- I never wanted a problem with lining up a dropdown.... i'm sad -->
 <!-- this launches another php file --->
-  <form class="form-horizontal" action="includes/users.php" method="post">
+  <form class="form-horizontal" action="Assign-Curriculum.php" method="post">
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
 		<div class="form-group">
@@ -61,19 +106,9 @@
 					<input type="text" class="form-control input-lg" placeholder="last name" name="l_name" id="attendance-l_name" oninput="validateAlpha('attendance-l_name');">
 				</div>
 			<label for="sel1">Select A Curriculum:</label> 
-				<select class="form-control" id="sel1">
+				<select class="form-control" id="sel1" name="curriculum_select">
 					<?php
-						session_start();
-	
-						if (!isset($_SESSION["username"]) ){
-							header('Location: index.php');
-							echo "hello";
-						}
-					  
-					  
-						// Connecting, selecting database
-						$dbconn = pg_connect("host=10.10.7.195 port=5432 dbname=cappingdb user=postgres password=admin")
-							or die('Could not connect: ' . pg_last_error());
+						
 
 						// Performing SQL query
 						$query = 'SELECT * FROM public.curriculum ORDER BY cid ASC ';
@@ -83,6 +118,7 @@
 					while($row = pg_fetch_array($result)){
 						echo "<option value='".$row['curriculum_name']."'>".$row['curriculum_name']."</option>";
 					}
+					
 					?>
 				</select>
 		</div>
@@ -91,7 +127,7 @@
 
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Submit</button>
+      <button type="submit" name="testName" class="btn btn-default">Submit</button>
 	  <!-- needs apache/php link to database -->
 	  <!-- needs to send user back to login once new password is created -->
 	  <!-- style is messed up will fix tomorrow --> 
